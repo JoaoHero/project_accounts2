10db601e4133e12828aa6f133adc5f57fcef4fde
João vitor Silva Ramos <joao.silvaramos2013@hotmail.com>
2024-02-14 13:10:04 -0300

    Criado a section de analise da home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,12 @@ import Container from "../components/Container";
 import SectionApp from "../components/SectionApp";
 
 import creditCarImg from "../assets/img/img-pages/card.png";
-import logoClientImg from "../assets/img/logos/LogoClient.png"
+import logoClientImg from "../assets/img/logos/LogoClient.png";
+import graphicImg from "../assets/img/img-pages/graphic.png";
+import featureAnalitic from "../assets/img/img-pages/featureAnalitic.png";
+import featureMove from "../assets/img/img-pages/featureMove.png";
+import featureSend from "../assets/img/img-pages/featureSend.png";
+
 
 function Home() {
     return(
@@ -49,6 +54,34 @@ function Home() {
                 </section>
 
                 <SectionApp />
+
+                <section className="section-analytics">
+                    <h1>Na palma da sua mão.</h1>
+
+                    <div className="section-analytics-wrapper">
+                        <div className="section-container-left">
+                            <img src={graphicImg} alt="graphic-img" />
+                        </div>
+
+                        <div className="section-container-right">
+                            <div className="container-right-description">
+                                <h1>Nosso compromisso com a inovação</h1>
+                                <p>
+                                    No mundo acelerado de hoje em dia, administar suas finanças sem nenhum tipo de tecnologia seria desgastante. 
+                                    Diga adeus para as grandes filas e horas gastadas em bancos. Nosso aplicativo móvel, 
+                                    de última geração está aqui para trasnformar a forma em que você interage e conhece a suas dívidas.
+                                </p>
+                            </div>
+
+                            <div className="container-right-img">
+                                <img src={featureAnalitic} alt="feature-analitic-img" />
+                                <img src={featureMove} alt="feature-move-img" />
+                                <img src={featureSend} alt="feature-send-img" />
+                            </div>
+                        </div>
+                    </div>
+ 
+                </section>
 
             </main>
 
