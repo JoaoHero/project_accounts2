80238649ac0242029e49698eec183f084ca0bb15
João vitor Silva Ramos <joao.silvaramos2013@hotmail.com>
2024-02-13 22:44:40 -0300

    Em App.js foi alterado as rotas de contato e login, deixando vazias ja que as mesmas ainda nao foram criadas

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/about" element={<About />}></Route>
-                <Route path="/contact" element={<About />}></Route>
-                <Route path="/login" element={<About />}></Route>
+                <Route path="/contact" element=""></Route>
+                <Route path="/login" element=""></Route>
             </Routes>
       </Router>
     </div>
